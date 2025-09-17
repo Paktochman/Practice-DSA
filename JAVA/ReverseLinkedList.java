adfb0b88d2320bfaef173d07e8062377ca3f9ee7
Priyanka Katoch <priyankakatoch@Priyankas-MacBook-Air.local>
2025-09-17 19:22:43 +0530

    Add Java solution to reverse a singly linked list

---
+++ JAVA/ReverseLinkedList.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev; // new head
+    }
+
+    // Helper to print list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // Example 1: 1->2->3->4->5
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+
+        head1 = reverseList(head1);
+        printList(head1); // Output: 5->4->3->2->1->null
+
+        // Example 2: 6->8
+        ListNode head2 = new ListNode(6);
+        head2.next = new ListNode(8);
+
+        head2 = reverseList(head2);
+        printList(head2); // Output: 8->6->null
+    }
+}
